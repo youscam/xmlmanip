f091506b27ea0d788ddf4559a96afe92a2e846bd
kamal azzouzi <youscam@outlook.com>
2018-10-19 21:01:54 +0100

    #first commit

---
+++ xmlmanip/src/main/java/ma/youscam/xmlmanip/App.java
@@ -0,0 +1,13 @@
+package ma.youscam.xmlmanip;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
